b397427fa3db298cde4c281b01ab4472937ed0f5
Sefi Cohen <seficohen1@gmail.com>
2021-10-14 12:41:31 +0200

    added styling to directory and menue-item components

--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -10,28 +10,35 @@ class Directory extends Component {
       sections: [
         {
           title: 'BEDROOM',
-          image: 'https://ibb.co/2ZVRY8f',
+          image:
+            'https://images.unsplash.com/photo-1566665797739-1674de7a421a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1974&q=80',
           id: 1,
         },
         {
-          title: 'BATH',
-          image: 'https://i.ibb.co/x5NyWL8/bathroom.jpg',
+          title: 'BATHROOM',
+          image:
+            'https://images.unsplash.com/photo-1600566752355-35792bedcfea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1887&q=80',
           id: 2,
         },
         {
           title: 'KITCHEN',
-          image: 'https://ibb.co/2FV02wp',
+          image:
+            'https://images.unsplash.com/photo-1596205250168-c3583813eea0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
           id: 3,
         },
         {
           title: 'OUTDOOR & MORE',
-          image: 'https://ibb.co/JpbHYZ3',
+          image:
+            'https://images.unsplash.com/photo-1585917641362-9fbb6fcfc371?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1207&q=80',
           id: 4,
+          size: 'large',
         },
         {
           title: 'LIVING ROOM',
-          image: 'https://ibb.co/VL6fX9F',
+          image:
+            'https://images.unsplash.com/photo-1600121848594-d8644e57abab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
           id: 5,
+          size: 'large',
         },
       ],
     };
@@ -39,9 +46,9 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, image, id }) => {
+        {this.state.sections.map(({ title, image, id, size }) => {
           console.log(title, image, id);
-          return <MenuItem key={id} title={title} image={image} />;
+          return <MenuItem key={id} title={title} image={image} size={size} />;
         })}
       </div>
     );
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './menu-item.scss';
 
-const MenuItem = ({ title, image }) => {
+const MenuItem = ({ title, image, size }) => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${image})`,
-      }}
-      className="menu-item"
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${image})`,
+        }}
+      />
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SEE MORE</span>
