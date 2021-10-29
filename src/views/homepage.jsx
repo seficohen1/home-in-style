4634d67c51be082f6de50deae524e7290b09651e
Sefi Cohen <seficohen1@gmail.com>
2021-10-29 17:08:17 +0200

    changed to correct dependency -  styled component

--- src/views/homepage.jsx
+++ src/views/homepage.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Directory from '../components/directory/directory';
-import './hompage.scss';
+// import './hompage.scss';
+// import { HomePageContainer } from './hompage.styles';
+import styled from 'style-component';
+
+const HomePageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 80px;
+`;
 
 const Homepage = () => {
   return (
-    <div className="homepage">
+    <HomePageContainer>
       <Directory />
-    </div>
+    </HomePageContainer>
   );
 };
 
---
+++ src/views/hompage.styles.jsx
@@ -0,0 +1,7 @@
+import styled from 'style-component';
+
+export const HomePageContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
